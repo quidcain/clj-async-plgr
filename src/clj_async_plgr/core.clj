1e1eb0dc0a509f943c186c805b42f3856b0dac8a
Daniel Radezky <danielradezky@gmail.com>
2020-11-29 12:25:44 +0300

    Test altsgit add src/ with thread
    REPL hangs

--- src/clj_async_plgr/core.clj
+++ src/clj_async_plgr/core.clj
@@ -2,20 +2,10 @@
 
 (require '[clojure.core.async :as async :refer :all])
 
-(let [c (chan)]
-  (thread (>!! c "hello"))
-  (assert (= "hello" (<!! c)))
-  (close! c))
-
-(let [c (chan)]
-  (thread (Thread/sleep 3000) (>!! c "hello"))
-  (println (<!! c))
-  (close! c))
-
 (let [c1 (chan)
       c2 (chan)]
-    (thread (while true
-            (let [[v ch] (alts!! [c1 c2])]
-              (println "Read" v "from" ch))))
-    (>!! c1 "hi")
-    (>!! c2 "there"))
+  (thread 
+    (let [[v ch] (alts!! [c1 c2])]
+      (println "Read" v "from" ch)))
+  (>!! c1 "hi")
+  (>!! c2 "there"))
