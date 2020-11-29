8d2ab3ef35c071db03ed6653d8df6fd07bb6082e
Daniel Radezky <danielradezky@gmail.com>
2020-11-29 12:50:37 +0300

    Make the first attempt to implement throughput channel (hereinafter TC)
    The idea is to perform async or blocking actions with some throughput
    (delay between actions) and a global timeout after which code should
    stop performing new actions.
    In this attempty I realized that there is no sense in two different
    channels (throughput-c and timeout-c)

--- src/clj_async_plgr/core.clj
+++ src/clj_async_plgr/core.clj
@@ -2,13 +2,16 @@
 
 (require '[clojure.core.async :as async :refer :all])
 
-(def c (chan))
-(go 
-  (Thread/sleep 5000)
-  (>! c :timeout)
-  (close! c))
-(<!! c)
-
+(def throughput-c (chan))
+(def timeout-c (chan))
 (def timeout-future
-  (future (Thread/sleep 5000)))
-(realized? timeout-future)
+  (future
+    (Thread/sleep 5000)
+    (>! timeout-c :timeout)
+    (close! timeout-c)))
+
+(defn get-data[]
+  (when (not (realized? timeout-future))
+    (fn [x]
+      (>>! throughput-c x))
+    (alts!! [c1 c2]))
