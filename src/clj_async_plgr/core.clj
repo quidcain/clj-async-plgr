1b83e34fef0208b60b4fecc60e17f31f889c7a7c
Daniel Radezky <danielradezky@gmail.com>
2020-12-01 10:01:54 +0300

    TC Rewrite functions to parameterized

--- src/clj_async_plgr/core.clj
+++ src/clj_async_plgr/core.clj
@@ -2,24 +2,6 @@
   (:require
     [clojure.core.async :as a]))
 
-(def throughput-c (a/chan))
-(def timeout-future
-  (future 
-    (Thread/sleep 1000)))
-
-(defn a-delay []
-  (go
-    (<! (a/timeout 1000))
-    (>! throughput-c :ready)))
-
-(defn w-thrpt-and-timeout [f & args]
-  (if (realized? timeout-future)
-    :timeout
-    (do
-      (<!! throughput-c)
-      (a-delay)
-      (apply f args))))
-
 (defn get-reviews []
   (Thread/sleep 2500)
   {:reviews [
@@ -33,12 +15,29 @@
    :author "Eric"
    :rating 4.3})
 
-(put! throughput-c :ready)
-#_(let [get-reviews (partial w-thrpt-and-timeout get-reviews)
-      reviews (get-reviews)]
-  (if-not (= reviews :timeout)
-    reviews
-    :timeout))
+(defn timeout-future [ms]
+  (future
+    (Thread/sleep 1000)))
 
-((partial w-thrpt-and-timeout get-reviews))
-((partial w-thrpt-and-timeout (fn [] 4)))
+(defn a-delay [c]
+  (go
+    (<! (a/timeout 1000))
+    (>! c :ready)))
+
+(defn w-thrpt-and-timeout [ftr c f & args]
+  (if (realized? ftr)
+    :timeout
+    (do
+      (<!! c)
+      (a-delay c)
+      (apply f args))))
+
+(let [throughput-c (a/chan)
+      timeout-future (timeout-future 1000)
+      w-thrpt-and-timeout (partial
+        w-thrpt-and-timeout
+        timeout-future
+        throughput-c)
+      get-reviews (partial w-thrpt-and-timeout get-reviews)]
+  (put! throughput-c :ready)
+  (get-reviews))
